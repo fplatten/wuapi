217f90439cb65451f98f019cab5e1db1b21e3902
fplatten <fplatten@gmail.com>
2015-05-07 18:29:57 -0400

    adding geoloc unit test

--- src/test/java/com/first/wuapi/ServiceImplTest.java
+++ src/test/java/com/first/wuapi/ServiceImplTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import com.first.wuapi.domain.WUElement;
 import com.first.wuapi.domain.conditions.ConditionsResult;
 import com.first.wuapi.domain.conditions.CurrentObservation;
+import com.first.wuapi.domain.geolookup.GeoLookupResult;
+import com.first.wuapi.domain.geolookup.Location;
 import com.first.wuapi.services.WUndergroundService;
 import com.first.wuapi.services.impl.WUndergroundServiceImpl;
 
@@ -32,6 +34,29 @@ public class ServiceImplTest {
 		
 		
 		assertEquals( "KCASANFR58"  , co.getStationId());
+		
+		
+	}
+	
+	@Test
+	public void testGeoLookup(){
+		
+		String feature = "geolookup";
+		String settings = "CA";
+		String query = "San_Francisco";
+		String format = "json";
+		
+		WUndergroundService wUndergroundService = new WUndergroundServiceImpl();
+		
+		WUElement element = wUndergroundService.query(feature, settings, query, format);
+		
+		assertEquals( GeoLookupResult.class  , element.getClass());
+		
+		GeoLookupResult gl = (GeoLookupResult) element;
+		
+		Location loc = gl.getLocation();
+		
+		assertEquals( "94101"  , loc.getZip());
 		
 		
 	}
